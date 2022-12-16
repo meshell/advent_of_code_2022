d8324fe04f8463704b9da5b236657616af44b102
Michel Estermann <michel.estermann@komaxgroup.com>
2022-12-16 18:48:39 +0100

    Day 6 part 2

--- day6/lib/day6.ex
+++ day6/lib/day6.ex
@@ -2,16 +2,22 @@ defmodule Day6 do
   @moduledoc """
   Documentation for Day6.
   """
-
-  def find_marker_window({x, []}) do
+  def find_marker_window({x, []}, m) do
     {x, []}
   end
 
-  def find_marker_window({x, y}) do
+  def find_marker_window({x, y}, m) do
     [h | tail] = y
 
-    if length(Enum.take(y, 4)) == length(Enum.uniq(Enum.take(y, 4))) do
-      {x ++ Enum.take(y, 4), []}
+    n =
+      if m == :start_of_message do
+        14
+      else
+        4
+      end
+
+    if length(Enum.take(y, n)) == length(Enum.uniq(Enum.take(y, n))) do
+      {x ++ Enum.take(y, n), []}
     else
       {x ++ [h], tail}
     end
@@ -23,29 +29,36 @@ defmodule Day6 do
 
   ## Examples
 
-      iex> "mjqjpqmgbljsphdztnvjfqwrcgsmlb" |> Day6.find_marker()
+      iex> "mjqjpqmgbljsphdztnvjfqwrcgsmlb" |> Day6.find_marker(:start_of_packet)
       7
-      iex> "bvwbjplbgvbhsrlpgdmjqwftvncz" |> Day6.find_marker()
+      iex> "bvwbjplbgvbhsrlpgdmjqwftvncz" |> Day6.find_marker(:start_of_packet)
       5
-      iex> "nppdvjthqldpwncqszvftbrmjlhg" |> Day6.find_marker()
+      iex> "nppdvjthqldpwncqszvftbrmjlhg" |> Day6.find_marker(:start_of_packet)
       6
-      iex> "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" |> Day6.find_marker()
+      iex> "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" |> Day6.find_marker(:start_of_packet)
       10
-      iex> "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" |> Day6.find_marker()
+      iex> "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" |> Day6.find_marker(:start_of_packet)
       11
+      iex> "mjqjpqmgbljsphdztnvjfqwrcgsmlb" |> Day6.find_marker(:start_of_message)
+      19
   """
-  def find_marker(input) do
+  def find_marker(input, m) do
     input_list =
       input
       |> String.codepoints()
 
     input_list
-    |> Enum.reduce({[], input_list}, fn _, acc -> find_marker_window(acc) end)
+    |> Enum.reduce({[], input_list}, fn _, acc -> find_marker_window(acc, m) end)
     |> (fn {x, _} -> length(x) end).()
   end
 
   def main(args) do
-    find_marker(Input.get_input())
+    {options, _, _} = OptionParser.parse(args, strict: [part: :integer])
+
+    case options do
+      [part: 1] -> find_marker(Input.get_input(), :start_of_packet)
+      _ -> find_marker(Input.get_input(), :start_of_message)
+    end
     |> IO.inspect()
   end
 end
